60d2e6624b1257468d91af93bf0a6060d27ac074
MANFRED OBUMMA <FREDOBUMMA@gmail.com>
2022-10-10 00:33:57 +0100

    --feat: creating module/s for sending http request

---
+++ src/services/logService.js
@@ -0,0 +1,19 @@
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn: "https://6692dac13c014a079fde31465368dac0@o4503936594804736.ingest.sentry.io/4503956550647808",
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log,
+};
